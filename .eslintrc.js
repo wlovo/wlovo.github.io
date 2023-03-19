e39a94ad01340dc03a6562ca120ffd9859fcad22
Will Lovo <will9637@gmail.com>
2023-03-19 08:58:56 -0400

    Update dependencies and configs
    Updates package dependencies and various configuration values.
    As a bonus, static files are updated.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "react-app",
+    "react-app/jest",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
