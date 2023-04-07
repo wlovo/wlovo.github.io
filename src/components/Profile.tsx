caf594bc6e3565f39fb4b8d20b57bd600f7f8621
Will Lovo <will9637@gmail.com>
2023-04-07 19:22:58 -0400

    Update Profile section
    - Utilizes `JsonifiedFragment` utility component
    - Rework profile object to include more relavant information
    - Adds an updated Resume file as a static asset
    - Add Resume to profile object and links the file
    - Reformat for readability

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,40 +1,42 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import Resume from "../files/Resume-4.0.pdf";
+import JsonifiedFragment, { KeyedLink } from "./JsonifiedFragment";
 import Section from "./Section";
 
-const profile: { [key: string]: string | string[] } = {
+import moment from "moment";
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const experienceDuration = moment.duration(moment().diff(moment("2019-06-01")))
+
+const profile = {
   fullName: "William H. Lovo",
   location: "Harrisonburg, VA",
-  education: "James Madison University",
-  major: "Computer Science",
-  minors: ["Mathematics", "Data Analytics"],
-  graduated: "May 2020",
-  research: "Machine Learning",
+  education: {
+    name: "James Madison University",
+    major: "Computer Science",
+    research: "Machine Learning",
+    graduated: "May 2020",
+  },
+  experience: {
+    active: `${experienceDuration.years()} years and ${experienceDuration.months()} months`,
+    mostRecent: {
+      company: "Amazon Web Services",
+      title: "Software Development Engineer II",
+      since: "July 2020",
+    },
+  },
 };
 
-const links: string[][] = [
-  ["github", "https://github.com/wlovo", "wlovo"],
-  ["linkedin", "https://linkedin.com/in/william-lovo/", "William Lovo"],
-  ["research", "https://dl.acm.org/doi/10.1145/3323994.3369892", "POST"],
+const links: KeyedLink[] = [
+  { key: "github", display: "wlovo", link: "https://github.com/wlovo" },
+  { key: "linkedin", display: "William Lovo", link: "https://linkedin.com/in/william-lovo" },
+  { key: "research", display: "POST", link: "https://dl.acm.org/doi/10.1145/3323994.3369892" },
 ];
 
+const resume: KeyedLink = { key: "resume", display: "Resume", link: Resume as string };
+
 const Profile = (): JSX.Element => {
-  const profileText = JSON.stringify(profile, null, 2);
-  const linkText = (
-    <>
-      {"{\n"}
-      {links.map(([key, link, display], index) => (
-        <React.Fragment key={key}>
-          {`  "${key}": "`}
-          <a target="_blank" rel="nofollow noopener noreferrer" href={link}>
-            {display}
-          </a>
-          {index < links.length - 1 ? '",\n' : '"\n'}
-        </React.Fragment>
-      ))}
-      {"}"}
-    </>
-  );
+  const profileFragment = JSON.stringify(profile, null, 2);
 
   return (
     <Section id="profile" className="profile">
@@ -42,17 +44,14 @@ const Profile = (): JSX.Element => {
         <Container className="console">
           <p className="entry">const wlovo = Person.find(&quot;William Lovo&quot;)</p>
           <p className="entry">wlovo.getProfile()</p>
-          <pre className="response">{profileText}</pre>
+          <pre className="response">{profileFragment}</pre>
           <p className="entry">wlovo.getLinks()</p>
-          <pre className="response">{linkText}</pre>
+          <pre className="response">
+            <JsonifiedFragment links={links} />
+          </pre>
           <p className="entry">wlovo.getResume()</p>
           <pre className="response">
-            Coming soon!
-            {/* {'{\n  "resume": "'}
-            <a target="_blank" rel="nofollow noopener noreferrer" href={Resume}>
-              Resume
-            </a>
-            {'"\n}'} */}
+            <JsonifiedFragment links={[resume]} />
           </pre>
           <p className="entry">wlovo.nextProspect()</p>
         </Container>
