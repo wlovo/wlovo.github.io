c29fee876e0f6c70bd1e95ac33b2c6aaebfd86da
Will Lovo <will9637@gmail.com>
2023-04-07 20:10:21 -0400

    Update Header
    - Adds Project link
    - Adds Profile picture as a Navbar thumbnail

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
+import ProPic from "../images/NewProPic.png";
+
 import React, { useState } from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Image, Nav, Navbar } from "react-bootstrap";
 
 const Header = (): JSX.Element => {
   const [toggled, setToggled] = useState(false);
 
   return (
     <Navbar expand={false}>
-      <Navbar.Brand href="/">William Lovo</Navbar.Brand>
+      <Navbar.Brand href="/" className="d-flex align-items-center">
+        <Image src={ProPic as string} alt="William Lovo" width="50" height="50" className="me-1" roundedCircle thumbnail />
+        William Lovo
+      </Navbar.Brand>
       <Navbar.Toggle
         aria-controls="navbar-nav"
         label="Toggle the navigation bar"
@@ -18,6 +23,7 @@ const Header = (): JSX.Element => {
           <Nav.Link href="#profile">Profile</Nav.Link>
           <Nav.Link href="#education">Education</Nav.Link>
           <Nav.Link href="#experience">Experience</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
