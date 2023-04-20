65213aa9877bbdcd9452fbb4d444f5abc3470d9c
Will Lovo <will9637@gmail.com>
2023-04-20 15:40:14 -0400

    Add new project
    - Adds new project card
    - Minor display tweaks

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,18 @@
+import BW from "../images/blog-website.png";
+import FD from "../images/detecting-credit-card-fraud.png";
+import PW from "../images/profile-website-pic.png";
 import Section from "./Section";
 import Tags, { TagProps } from "./Tags";
 
 import isArray from "lodash/isArray";
 import React from "react";
 import { Card, Col, Container, NavLink, Row } from "react-bootstrap";
-import FD from "../images/detecting-credit-card-fraud.png"
-import PW from "../images/profile-website-pic.png"
 
 interface ProjectCardProps extends TagProps {
   name: string;
-  img?: string;
   desc: string | string[];
-  links: [string, string][];
+  img?: string;
+  links?: [string, string][];
 }
 
 const projects: ProjectCardProps[] = [
@@ -28,19 +29,8 @@ const projects: ProjectCardProps[] = [
     tags: ["Typescript", "React", "Redux", "Bootstrap", "SASS"],
   },
   {
-    name: "Fraud Detection",
-    img: FD as string,
-    desc: [
-      "Fraud Detection paper.",
-      "Proposed model.",
-    ],
-    links: [
-      ["JMU Scholarly Commons", "https://commons.lib.jmu.edu/honors202029/86/"],
-    ],
-    tags: ["Python", "Keras", "TensorFlow", "Machine Learning", "Anomaly Detection", "Data Science", "Data Analytics"],
-  },
-  {
     name: "Blog App",
+    img: BW as string,
     desc: [
       "A sample full-stack blog app built with React, Redux, and Bootstrap for the front-end and Express, Sequelize, and MySQL for the back-end.",
       "Showcases the Request/Response process through a RESTful API and CRUD operations on a modeled MySQL database.",
@@ -50,6 +40,22 @@ const projects: ProjectCardProps[] = [
       ["GitHub (Node)", "https://github.com/wlovo/blog-app-backend"],
     ],
     tags: ["Javascript", "Node.js", "React", "Redux", "Bootstrap", "Sequelize", "Express", "MySQL"],
+  },
+  {
+    name: "Detecting credit card fraud",
+    img: FD as string,
+    desc: [
+      "The research paper summarizing my research on detecting credit card fraud, analyzing modern stratagies, and proposing additional strategies to improve detection.",
+      "Showcases the use of Python, Keras, TensorFlow, and various Machine Learning strategies to detect fraud.",
+    ],
+    links: [["JMU Scholarly Commons", "https://commons.lib.jmu.edu/honors202029/86/"]],
+    tags: ["Python", "Keras", "TensorFlow", "Machine Learning", "Anomaly Detection", "Data Science", "Data Analytics"],
+  },
+  {
+    name: "What's next?",
+    desc: "Check out my GitHub for more projects and projects I'm working on.",
+    links: [["My GitHub page", "https://github.com/wlovo"]],
+    tags: ["Other"],
   },
 ];
 
