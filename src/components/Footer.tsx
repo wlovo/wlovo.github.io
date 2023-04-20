33c752e08230330a18e2ad81c984cd9342307c73
Will Lovo <will9637@gmail.com>
2023-04-20 15:40:42 -0400

    Add contact links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 
+const links: [string, string][] = [
+  ["LinkedIn", "https://linkedin.com/in/william-lovo"],
+  ["GitHub", "https://github.com/wlovo"],
+];
+
 const Footer = () => (
   <footer>
     <Navbar>
       <Nav>
-        <Nav.Link href="#deets">More deets</Nav.Link>
-        <Nav.Link eventKey={2} href="#memes">
-          Dank memes
-        </Nav.Link>
+        <Navbar.Brand>You can find me on:</Navbar.Brand>
+        {links
+          ? links.map(([name, link]) => (
+              <Nav.Link target="_blank" rel="nofollow noopener noreferrer" href={link}>
+                {name}
+              </Nav.Link>
+            ))
+          : "Sorry, couldn't find my links!"}
       </Nav>
     </Navbar>
   </footer>
